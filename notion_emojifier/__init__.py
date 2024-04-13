7585c2551d4e5e4ebca2139b6425d5b33ec93ddb
Alex Thiele <alex@urbanmachine.build>
2024-04-13 16:20:51 -0700

    Scaffolding

--- notion_emojifier/__init__.py
+++ notion_emojifier/__init__.py
@@ -4,3 +4,4 @@ Use LLMs to add emojis to all your notion pages
 """
 
 __version__ = "0.1.0"
+from .main import main
---
+++ notion_emojifier/main.py
@@ -0,0 +1,9 @@
+from argparse import ArgumentParser
+
+
+def main():
+    parser = ArgumentParser()
+
+
+if __name__ == "__main__":
+    main()
