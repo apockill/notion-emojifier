a8741a07b44805f1371a4ca9d5f3279c0c816e59
Alex Thiele <alex@urbanmachine.build>
2024-04-13 19:06:29 -0700

    Linting

--- notion_emojifier/notion_helpers/schemas.py
+++ notion_emojifier/notion_helpers/schemas.py
@@ -1,4 +1,4 @@
-from typing import Literal, Any
+from typing import Any, Literal
 
 from openai import BaseModel
 
@@ -30,4 +30,20 @@ class Page(BaseModel):
 
     @property
     def title(self) -> str:
-        return self.properties[self.title_property_name].title[0].plain_text
+        try:
+            title_property = self.properties[self.title_property_name]
+        except KeyError as e:
+            raise ValueError(
+                "The 'Name' property for this page was not found. It's possible this "
+                "database has a non-default title property name. Please specify the "
+                "title property name in the 'title-property-name' script argument."
+            ) from e
+
+        if not isinstance(title_property, Name):
+            raise ValueError(
+                f"Expected the title property to contain information about the title "
+                f"of the page, but got {title_property}. Please make sure the title "
+                f"property is a 'Name' property, or override the 'title-property-name'"
+                f" script argument to point to the correct title property."
+            )
+        return title_property.title[0].plain_text
